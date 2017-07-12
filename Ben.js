ea1253e8924d6f04b50c878f78d34a68abc1d8e6
EbenezerASC4 <ebenezer.y16@outlook.com>
2017-07-12 11:25:43 -0700

    yanno the vibe./updated Ben.js

--- Ben.js
+++ Ben.js
@@ -1,2 +1,11 @@
 console.log ("Ben is awesome");
 console.log ("Hello world");
+
+function rand(num) {
+    //you  already know what time it is
+    var randDec = Math.random() * num;
+    var randInt = Math.floor(randDec);
+    console.log (randInt);
+    return randInt;
+}
+rand(50);
